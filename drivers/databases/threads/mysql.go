bc1bb26f3c35a15f6c18b8fb049cc69555ed3a51
M Ghifari <ghifarim68@gmail.com>
2022-01-06 11:01:32 +0700

    fixing bug searching

--- drivers/databases/threads/mysql.go
+++ drivers/databases/threads/mysql.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fgd-alterra-29/business/threads"
 	"fgd-alterra-29/drivers/databases/comments"
-	"fmt"
 
 	"gorm.io/gorm"
 )
@@ -122,12 +121,13 @@ func (DB *MysqlThreadRepository) GetCategories(id int, index int) *gorm.DB {
 func (DB *MysqlThreadRepository) GetSearch(ctx context.Context, threadname string) ([]threads.Domain, error) {
 	var Thread []Threads
 	var Comment comments.Comments
+	var Newthreadname = ("%" + threadname + "%")
 
 	Q_Like := DB.Conn.Table("thread_likes").Where("thread_id = threads.id").Select("count(thread_likes.user_id)").Group("thread_id")
 	Q_Comment := DB.Conn.Table("comments").Where("thread_id = threads.id").Select("count(comment)").Group("thread_id")
 
 	result := DB.Conn.Table("threads").Select("*, title, content, (?) as Q_Like, (?) as Q_Comment", Q_Like, Q_Comment).
-		Where("title = (?)", threadname).
+		Where("title LIKE ?", Newthreadname).
 		Preload("Comments", func(db *gorm.DB) *gorm.DB {
 			return db.Table("comments").Select("*").
 				Joins("join users on comments.user_id = users.id").Find(&Comment)
@@ -137,8 +137,6 @@ func (DB *MysqlThreadRepository) GetSearch(ctx context.Context, threadname strin
 	if result.Error != nil {
 		return []threads.Domain{}, result.Error
 	}
-
-	fmt.Println(ToListDomain(Thread))
 
 	return ToListDomain(Thread), nil
 }

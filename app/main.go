5929d173e8f99750d3c32dbf0316615e73c6336c
Ghynmo <ghifarim68@gmail.com>
2022-01-21 06:45:19 +0700

    fix configs

--- app/main.go
+++ app/main.go
@@ -116,7 +116,7 @@ func DbMigrate(db *gorm.DB) {
 
 func main() {
 
-	config, err := configs.LoadConfig("app/configs/config.go")
+	config, err := configs.LoadConfig("app/configs/")
 	if err != nil {
 		log.Fatal("cannot load config:", err)
 	}
